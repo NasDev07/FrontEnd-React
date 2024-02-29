165a0768a2f3eca0f624c27a47fa4cd693833482
NasDev07 <nasruddin20182019@gmail.com>
2024-02-29 12:24:55 +0700

    Rendering Lists

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,27 +1,42 @@
-import Cardproduct from "../components/Fragments/CardProduct";
+import CardProduct from "../components/Fragments/CardProduct";
+
+const Products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp. 1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quis eius molestiae dolores exercitationem ipsam a dolor fuga corporis odio?`,
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp. 500.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+  },
+  {
+    id: 2,
+    name: "Sepatu Nike",
+    price: "Rp. 700.000",
+    image: "/images/shoes-1.jpg",
+    description: `Ini adalah sepatu baru dari brand NIKE`,
+  },
+];
 
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <Cardproduct>
-        <Cardproduct.Header image="/images/shoes-1.jpg" />
-        <Cardproduct.Body title="Sepatu baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ab
-          recusandae odit amet, quia consequatur eligendi illum deserunt quod
-          tempora?
-        </Cardproduct.Body>
-        <Cardproduct.Footer price="Rp. 1.000.000" />
-      </Cardproduct>
-
-      <Cardproduct>
-        <Cardproduct.Header image="/images/shoes-1.jpg" />
-        <Cardproduct.Body title="Sepatu baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ab
-          recusandae odit amet, quia consequatur eligendi illum deserunt quod
-          tempora?
-        </Cardproduct.Body>
-        <Cardproduct.Footer price="Rp. 1.000.000" />
-      </Cardproduct>
+      {Products.map((product) => (
+        // eslint-disable-next-line react/jsx-key
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
