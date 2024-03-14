374e0981cbcae50298373f77cb652f7cbbe47004
NasDev07 <nasruddin20182019@gmail.com>
2024-03-14 14:06:41 +0700

    Post Data Api & login token jwt

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,6 +2,7 @@ import { Fragment, useEffect, useRef, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elemtnts/Button";
 import { getProducts } from "../services/product.service";
+import { getUsername } from "../services/auth.service";
 // import Counter from "../components/Fragments/Counter";
 
 // const Products = [
@@ -28,15 +29,23 @@ import { getProducts } from "../services/product.service";
 //   },
 // ];
 
-const email = localStorage.getItem("email");
-
 const ProductsPage = () => {
   const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
   const [products, setProducts] = useState([]);
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     setCart(JSON.parse(localStorage.getItem("cart")) || []);
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUsername(getUsername(token));
+    } else {
+      window.location.href = "/login";
+    }
   }, []);
 
   // get data api
@@ -58,8 +67,8 @@ const ProductsPage = () => {
   }, [cart, products]);
 
   const handleLogout = () => {
-    localStorage.removeItem("email");
-    localStorage.removeItem("password");
+    localStorage.removeItem("token");
+    // localStorage.removeItem("password");
     window.location.href = "/login";
   };
 
@@ -96,7 +105,7 @@ const ProductsPage = () => {
   return (
     <Fragment>
       <div className="flex justify-end h-16 bg-blue-600 text-white items-center px-10">
-        {email}
+        {username}
         <Button classname="ml-5 bg-black" onClick={handleLogout}>
           Logout
         </Button>
